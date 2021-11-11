24ac4de5633ab571292b2def6325626c0a592d72
Rich <rich@posert.com>
2021-11-11 08:34:22 -0800

    Finish day six. Easy!

--- Day-06/six.py
+++ Day-06/six.py
@@ -1,6 +1,6 @@
 from string import ascii_lowercase
 
-def read_declaration(combined_declaration):
+def part_one(combined_declaration) -> int:
     declaration = 0
 
     for letter in ascii_lowercase:
@@ -9,6 +9,19 @@ def read_declaration(combined_declaration):
 
     return declaration
 
+def part_two(declaractions) -> int:
+    letters = ascii_lowercase
+    remaining_letters = []
+
+    for declaration in declaractions:
+        for letter in letters:
+            if letter in declaration:
+                remaining_letters.append(letter)
+        letters = remaining_letters
+        remaining_letters = []
+
+    return len(letters)
+
 declarations = []
 with open('input.txt', 'r') as f:
     curr_dec = []
@@ -16,9 +29,13 @@ with open('input.txt', 'r') as f:
         if line != '\n':
             curr_dec.append(line.rstrip())
         else:
-            declarations.append(read_declaration(''.join(curr_dec)))
+            declarations.append(curr_dec)
             curr_dec = []
     
-    declarations.append(read_declaration(''.join(curr_dec)))
+    declarations.append(curr_dec)
 
-print(sum(declarations))
+part_one = sum([part_one(''.join(x)) for x in declarations])
+part_two = sum([part_two(x) for x in declarations])
+
+print(part_one)
+print(part_two)

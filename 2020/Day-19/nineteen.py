ba5683a958c95268785bf4ed0d373cefe21120f1
Rich <rich@posert.com>
2021-11-23 09:27:51 -0800

    Almost elegant

--- 2020/Day-19/nineteen.py
+++ 2020/Day-19/nineteen.py
@@ -12,16 +12,11 @@ for rule in rules:
     except ValueError:
         messages.append(rule)
 
-# for mess in messages:
-#     print(mess)
-
-for num, rule in rule_dict.items():
-    print(f'{num}: {rule}')
-
 compiled_rules = {}
 
 def compile_rule(index, rule):
     try:
+        # memoization baby!
         return compiled_rules[index]
 
     except KeyError:
@@ -38,9 +33,45 @@ def compile_rule(index, rule):
             result = ''.join(rule_list)
             result = f'(?:{result})'
             compiled_rules[index] = result
-            print(compiled_rules)
 
             return result
+
+# part one
+
+rule_zero = re.compile(f'^{compile_rule("0", rule_dict["0"])}$')
+
+matches = [re.match(rule_zero, x) for x in messages]
+
+print(len([x for x in matches if x]))
+
+# part two
+# 8: 42 | 42 8 is the same as 8: (42){1,}
+#
+# 11: 42 31 | 42 11 31 is the same as 11: ((42){1,} (31){1,})
+# technically, here, these would have to match the same number
+# of times...hopefully the puzzle is kind
+# It's not...
+#
+# Hmm...maybe I should just be lazy...
+# 
+# so I can first solve 42 and 31, then hardcode the new rules.
+# thank god for regex
+
+compiled_rules = {}
+compile_rule('31', rule_dict['31'])
+compile_rule('42', rule_dict['42'])
+ft = compiled_rules['42']
+to = compiled_rules['31']
+
+# hacky bullshit. Just make a big group that solves the same-repeat problem.
+# basically (aabb|aaaabbbb|aaaaaabbbbbb|aaaaaaaabbbbbbbb|etc). Only goes to eleven.
+eleven_list = []
+for i in range(10):
+    eleven_list.append(ft*(i + 1) + to * (i + 1))
+
+compiled_rules['8'] = compiled_rules['42'] + '{1,}'
+compiled_rules['11'] = '(?:' + '|'.join(eleven_list) + ')'
+
 
 rule_zero = re.compile(f'^{compile_rule("0", rule_dict["0"])}$')
 

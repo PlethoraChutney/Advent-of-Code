c88112a4ba1cb3ce4ac0a9f5a12f42f9bb154ff1
Rich <rich@posert.com>
2021-12-13 06:45:05 -0800

    Comments

--- 2021/Day-13/thirteen.py
+++ 2021/Day-13/thirteen.py
@@ -32,13 +32,18 @@ manual[np.index_exp[tuple(coords)]] = 1
 
 def fold_manual(direction, index):
     if direction == 'u':
+        # select top up to index
         top = manual[:index,:]
+        # select bottom up to index, reversing the order of rows to simulate a fold
         bottom = manual[:index:-1,:]
+        # it turns out this check isn't necessary, but theoretically I could have had
+        # to write a quick catch here to pad the top or bottom with zeros
         if top.shape == bottom.shape:
             return np.bitwise_or(top, bottom)
         else:
             raise IndexError
     if direction == 'l':
+        # same thing but left or right
         left = manual[:,:index]
         right = manual[:,:index:-1]
         if left.shape == right.shape:
@@ -46,15 +51,19 @@ def fold_manual(direction, index):
         else:
             raise IndexError
 
+# part one answer
 manual = fold_manual(*instructions[0])
-
 print(sum(manual.flatten()))
 
+# run the rest of the folds
 for i in range(1, len(instructions)):
     manual = fold_manual(*instructions[i])
 
+# need to replace the newlines here because numpy inserts newlines even if you
+# set the maximum print width to infinite. Annoying.
 def print_man(manual):
     for row in manual:
         print(str(row).replace('0', '.').replace('1', '#').replace('\n', ''))
 
+# part two answer is an image of letters made by the # symbols in the output
 print_man(manual)
